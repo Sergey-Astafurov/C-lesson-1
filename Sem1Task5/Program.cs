6b2add9a5ca79279c5735d2f763b02a054cc46c7
Sergey Astafurov <stupidsociety16@gmail.com>
2022-09-01 21:25:15 +0500

    семинар

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿//на вход принимает число и на выходе показывает все целые числа от -н до н
+
+string? inputLineN = Console.ReadLine();
+if (inputLineN != null)
+{
+   int inputNumberN = int.Parse(inputLineN);
+
+   int startNumber = (-1) * inputNumberN;
+   while (startNumber < inputNumberN)
+   {
+      Console.Write(startNumber + ", ");
+      startNumber++;
+   }
+   Console.Write(inputNumberN);
+
+}
---
+++ Sem1task7/Program.cs
@@ -0,0 +1,15 @@
+﻿//на вход принимает трехзначное число и на выходе показывает его последнюю цифру
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+   int inputNumberN = int.Parse(inputLine);
+   if(inputNumberN > 99 && inputNumberN < 1000)
+   {
+      int lastDigit = inputNumberN % 10;
+       Console.Write(lastDigit);
+   }
+
+
+
+}
