575fd3beac11dbf1f78340ec72ddfa8541cd036e
Sergey Astafurov <stupidsociety16@gmail.com>
2022-09-01 22:22:14 +0500

    number 6

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿//№4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+string? inputLine = Console.ReadLine(); //Ввод числа
+if (inputLine != null) //Проверка чтобы поле было заполнено
+{
+   int inputNumber = int.Parse(inputLine); //перевод строки в число
+   if (inputNumber % 2 == 0)
+   { // применяем остаток от деления
+      Console.WriteLine("чётное");
+   }
+   else
+   {
+      Console.WriteLine("нечётное");
+   }
+}
+
+
+
