16c6c36156545950cdfd5e1583c54bc3dc52c59f
Sergey Astafurov <stupidsociety16@gmail.com>
2022-09-01 21:49:30 +0500

    number 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// №2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+string? inputLineA = Console.ReadLine(); //Ввод первого числа
+string? inputLineB = Console.ReadLine(); //Ввод второго числа
+int max = 0;
+if (inputLineA != null && inputLineB != null) //Проверка чтобы поле было заполнено
+{
+   int inputNumberA = int.Parse(inputLineA); //перевод строки в число
+   int inputNumberB = int.Parse(inputLineB); //перевод строки в число
+
+   if (inputNumberA == inputNumberB){ //проверка равны ли числа
+      Console.WriteLine("они равны");
+   }else if (inputNumberA > inputNumberB){ // если первое больше второго,то оно и будет максимальным
+      max = inputNumberA;
+      Console.WriteLine("max = " +  max);
+
+   }else{ //иначе второе
+      max = inputNumberB;
+      Console.WriteLine("max = " +  max);
+
+   }
+
+
+}
